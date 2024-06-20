39e6e24c2f529968102f0fb71ecec67d1da22779
bidaliya <bidaliyatarun427@gmail.com>
2024-06-20 22:28:48 +0530

    solved bugs

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -34,8 +34,9 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           onClick={handleProfileClick}
         >
           <Image
-            src={post.creator.image}
+            src={post.creator ? post.creator.image : ""}
             alt='user_image'
+            // placeholder={}
             width={40}
             height={40}
             className='rounded-full object-contain'
@@ -43,10 +44,10 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
           <div className='flex flex-col'>
             <h3 className='font-satoshi font-semibold text-gray-900'>
-              {post.creator.username}
+              {post.creator?.username ?? ""}
             </h3>
             <p className='font-inter text-sm text-gray-500'>
-              {post.creator.email}
+              {post.creator?.email ?? ""}
             </p>
           </div>
         </div>
@@ -73,7 +74,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         #{post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
             className='font-inter text-sm green_gradient cursor-pointer'
